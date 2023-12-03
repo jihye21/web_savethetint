1a0755b74a4e14cdc0ce976db85ec31d62ee16af
jihye21 <112855236+jihye21@users.noreply.github.com>
2023-12-03 19:23:26 +0900

    Update register.php

--- register.php
+++ register.php
@@ -7,9 +7,6 @@ $dbname = "stt_data";
 
 // 연결 생성
 $conn = new mysqli($servername, $username, $password, $dbname);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // 연결 확인
 if ($conn->connect_error) {
@@ -17,43 +14,37 @@ if ($conn->connect_error) {
 }
 
 // 폼에서 사용자 입력 가져오기
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // 비밀번호를 안전하게 해싱
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
 
-// 사용자 데이터를 데이터베이스에 삽입
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    // 비밀번호 확인
+    if ($password != $confirmPassword) {
+        die("비밀번호가 일치하지 않습니다.");
+    }
 
-if ($conn->query($sql) === TRUE) {
-    echo "회원가입이 완료되었습니다.";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    // 이미 있는 아이디인지 체크
+    $checkDuplicateSql = "SELECT id FROM users WHERE email = '$email'";
+    $duplicateResult = $conn->query($checkDuplicateSql);
 
-// 데이터베이스 연결 종료
-$conn->close();
-?>
+    if ($duplicateResult->num_rows > 0) {
+        die("이미 사용 중인 아이디입니다.");
+    }
 
-// 연결 생성
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // 비밀번호를 안전하게 해싱
 
-// 연결 확인
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // 사용자 데이터를 데이터베이스에 삽입
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashedPassword')";
 
-// 폼에서 사용자 입력 가져오기
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // 비밀번호를 안전하게 해싱
-
-// 사용자 데이터를 데이터베이스에 삽입
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "회원가입이 완료되었습니다.";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        echo "회원가입이 완료되었습니다.";
+        header("Location: test.index.html");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
 // 데이터베이스 연결 종료
